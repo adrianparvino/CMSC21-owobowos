986cddc265059c948c40427e5b99606ae4130366
Adrian Parvin Ouano <me@adrianpar.vin>
2025-05-21 01:23:17 +0800

    Add played counter

--- main.cxx
+++ main.cxx
@@ -1,8 +1,34 @@
+#include <fstream>
 #include <iostream>
 
-int main()
-{
-    std::cout << "Hello World!" << std::endl;
+class PersistentCounter {
+  public:
+    PersistentCounter(const char *filename) : filename(filename) {
+        std::ifstream file(filename);
+        if (file.is_open()) {
+            file >> counter;
+        }
+    }
+
+    int postIncrement() { return counter++; }
+
+    ~PersistentCounter() {
+        std::ofstream file(filename);
+
+        if (file.is_open()) {
+            file << counter << std::endl;
+        }
+    }
+
+  private:
+    std::string filename;
+    int counter = 0;
+};
+
+int main() {
+    static PersistentCounter counter("played.txt");
+
+    std::cout << "Played " << counter.postIncrement() << " times." << std::endl;
 
     return 0;
 }
